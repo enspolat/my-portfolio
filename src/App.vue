afa35637bd742a30a0d64d1f1171b4e0ebd8d312
Enes Polat <enespolat@Eness-MacBook-Air.local>
2024-10-20 23:23:43 +0300

    deleted trash

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,110 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <section class="flex items-center justify-center h-screen w-screen">
+    <div class="container">
+      <div class="cards-container">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div v-for="(card, index) in cards" :key="index">
+            <div class="single-card-wrapper">
+              <a href="#">
+                <div
+                  class="card-hover-container relative flex items-center justify-center rounded-lg border border-gray-700 overflow-hidden cursor-pointer min-h-[399px] aspect-square"
+                  @mousemove="onHover($event, index)"
+                  @touchmove="onHover($event, index)"
+                >
+                  <img :src="card.imgSrc" alt="" class="z-10" />
+                  <div
+                    class="absolute inset-0 bg-gradient-radial from-gray-900 via-indigo-500 to-pink-500 z-0"
+                  ></div>
+                  <div
+                    class="card-bg-characters absolute inset-0 text-sm font-mono text-white opacity-0 transition-opacity duration-500"
+                    :style="getCharacterStyle(index)"
+                  >
+                    {{ randomStrings[index] }}
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script lang="ts">
+import './assets/main.css'
+
+import { defineComponent, reactive } from 'vue'
+
+interface Card {
+  imgSrc: string
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+interface Position {
+  x: string
+  y: string
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+export default defineComponent({
+  setup() {
+    const cards: Card[] = reactive([
+      {
+        imgSrc:
+          'https://www.yudiz.com/codepen/code-hover//github-mark-white.svg',
+      },
+      { imgSrc: 'https://www.yudiz.com/codepen/code-hover//code.svg' },
+      { imgSrc: 'https://www.yudiz.com/codepen/code-hover//die.svg' },
+    ])
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+    const randomStrings = reactive<string[]>(['', '', ''])
+    const positions = reactive<Position[]>([
+      { x: '0px', y: '0px' },
+      { x: '0px', y: '0px' },
+      { x: '0px', y: '0px' },
+    ])
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+    const randomString = (length: number): string => {
+      const chars =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+      return Array.from(
+        { length },
+        () => chars[Math.floor(Math.random() * chars.length)],
+      ).join('')
+    }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+    const onHover = (event: MouseEvent | TouchEvent, index: number): void => {
+      const target = event.currentTarget as HTMLElement
+      const rect = target.getBoundingClientRect()
+      const x =
+        (event instanceof MouseEvent
+          ? event.clientX
+          : event.touches[0].clientX) - rect.left
+      const y =
+        (event instanceof MouseEvent
+          ? event.clientY
+          : event.touches[0].clientY) - rect.top
 
-nav a:first-of-type {
-  border: 0;
-}
+      positions[index] = { x: `${x}px`, y: `${y}px` }
+      randomStrings[index] = randomString(2000)
+    }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    const getCharacterStyle = (index: number): Record<string, string> => {
+      return {
+        '--x': positions[index].x,
+        '--y': positions[index].y,
+      }
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    return {
+      cards,
+      randomStrings,
+      positions,
+      randomString,
+      onHover,
+      getCharacterStyle,
+    }
+  },
+})
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style></style>
